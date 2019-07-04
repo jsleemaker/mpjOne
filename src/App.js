3dba9c294e081042ab70231a8f11e7b0d9dacc78
booski <p9346420@gmail.com>
2019-07-04 13:01:21 +0900

    Marker Ver2.0

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Place from './Place';
 let x = 37.580517;
 let y = 126.887714;
 
-// let markers = [];
-
-
+let markers = [];
 
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
@@ -18,21 +16,71 @@ class App extends Component {
     // 사이클의 시작
     // api등 원천 데이터 획득 작업 등
     console.log('will mount');
-
-    
+    this._getMapView();
 
   }
   
   componentDidMount(){
     // 컴포넌트들이 다 자리 잡음
     console.log('did mount');
-    this._getMapView();
-    // this._getMapInfo();
-
-    
   }
+
+  render(){
+    // 컴포넌트가 리액트 세계에 존재
+    console.log('did render');
+    const {places} = this.state;
+    // const {map} = this.state;
+  
+    return (
+      <div>
+        {/* <div id="map"></div> */}
+        <div className={places ? "Place":"Place--loading"}>
+          {places ? this._renderPlaces(this.state.map) : 'Loading'}
+        </div>
+      </div>
+    );
+  }
+
+  _getMapView(){
+    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    const options = { //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(x, y), //지도의 중심좌표.
+      level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+    this.setState({
+      map
+    })
+
+    kakao.maps.event.addListener(map, 'dragend', ()=>{
+
+      // 지도의  레벨을 얻어옵니다
+      const level = map.getLevel();
+  
+      // 지도의 중심좌표를 얻어옵니다 
+      const latlng = map.getCenter(); 
+  
+      let message = '<p>지도 레벨은 ' + level + ' 이고</p>';
+      message += '<p>중심 좌표는 위도 ' + latlng.getLat() + ', 경도 ' + latlng.getLng() + '입니다</p>';
+  
+      console.log(message);
+      x = latlng.getLat();
+      y = latlng.getLng();
+      // console.log(getM._getMapInfo(map));
+      // console.log(temp._getMapInfo(map));
+      console.log(this);
+      console.log(this._getMapInfo(map));
+    });
+    console.log(this);
+
+
+    this._getMapInfo(map);
+  }
+
+
   _getMapInfo = async(map)=>{
-    console.log('1111111')
     const places = await this._callMapApi();
     this.setState({
       places
@@ -46,16 +94,29 @@ class App extends Component {
       })
     })
     this._makeMakers(map, positions);
-    
-     
-    
   }
 
-  
+  _callMapApi= ()=>{
+    // const sort = 'distance';
+    const headers = {
+        'Authorization': 'KakaoAK 90c02250ba7baedbd7e0c3a29aa5d21c'
+    };
+
+
+    const url= 'https://dapi.kakao.com/v2/local/search/keyword.json?query=세차&x='+y+'&y='+x+'&sort=distance&radius=20000';
+    return fetch(url, {
+      headers: headers
+    })
+    .then(potato => potato.json())
+    .then(json =>json.documents)
+    .catch(err=> console.log(err))
+  }
+
   _makeMakers(map, positions){
     // 마커 이미지의 이미지 주소입니다
     const imageSrc = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
         
+    this._hideMarkers();
     for (var i = 0; i < positions.length; i ++) {
       // 마커 이미지의 이미지 크기 입니다
       var imageSize = new kakao.maps.Size(24, 35); 
@@ -71,90 +132,47 @@ class App extends Component {
           image : markerImage // 마커 이미지 
       });
       marker.setMap(map);
+      markers.push(marker);
     }  
   }
-  _callMapApi= ()=>{
-    const sort = 'distance';
-    const headers = {
-        'Authorization': 'KakaoAK 90c02250ba7baedbd7e0c3a29aa5d21c'
-    };
 
-
-    const url= 'https://dapi.kakao.com/v2/local/search/keyword.json?query=세차&x='+x+'&y='+y+'&sort='+sort;
-    return fetch(url, {
-      headers: headers
-    })
-    .then(potato => potato.json())
-    .then(json =>json.documents)
-    .catch(err=> console.log(err))
-  }
 
   state = {
     greeting : 'Hello',
     
   }
-  _getMapView(){
-    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    const options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(x, y), //지도의 중심좌표.
-      level: 3 //지도의 레벨(확대, 축소 정도)
-    };
 
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-    const mapDraw = this._getMapInfo();
-
-    kakao.maps.event.addListener(map, 'dragend', function(mapDraw) {
-
-      // 지도의  레벨을 얻어옵니다
-      var level = map.getLevel();
-  
-      // 지도의 중심좌표를 얻어옵니다 
-      var latlng = map.getCenter(); 
-  
-      var message = '<p>지도 레벨은 ' + level + ' 이고</p>';
-      message += '<p>중심 좌표는 위도 ' + latlng.getLat() + ', 경도 ' + latlng.getLng() + '입니다</p>';
-  
-      console.log(message);
-      x = latlng.getLat();
-      y = latlng.getLng();
-      
-
-    });
-    console.log(this)
-    this._getMapInfo(map);
+  // 배열에 추가된 마커들을 지도에 표시하거나 삭제하는 함수입니다
+  _setMarkers(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }            
   }
 
+
+  // "마커 감추기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에서 삭제하는 함수입니다
+  _hideMarkers() {
+    this._setMarkers(null);    
+  }
   
-  _renderPlaces = () =>{
+  _renderPlaces = (map) =>{
+    // this._getMapInfo(map);
     const places = this.state.places.map((place)=>{
       return <Place
-        place_name={place.place_name}
-        key={place.id}
-        phone={place.phone}
-        road_address={place.road_address_name}
-        address={place.address_name}
-        x={place.x}
-        y={place.y}
-        place_url={place.place_url} />
+      place_name={place.place_name}
+      key={place.id}
+      phone={place.phone}
+      road_address={place.road_address_name}
+      address={place.address_name}
+      x={place.x}
+      y={place.y}
+      place_url={place.place_url} />
     })
-
+    
     return places
   }
+  
 
-  render(){
-    // 컴포넌트가 리액트 세계에 존재
-    console.log('did render');
-    const {places} = this.state;
-    return (
-      <div>
-        <div id="map"></div>
-        <div className={places ? "Place":"Place--loading"}>
-          {places ? this._renderPlaces() : 'Loading'}
-        </div>
-      </div>
-    );
-  }
 
 
 
