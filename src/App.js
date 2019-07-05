560969ff84c02cd8ac3cb5e397ffc594c895b523
booski <p9346420@gmail.com>
2019-07-05 20:44:36 +0900

    Geolocation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import Place from './Place';
 
 /*global kakao*/
-let x = 37.580517;
-let y = 126.887714;
+let x = 36.332223;
+let y = 127.433706;
 
 let markers = [];
+
+
 
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
@@ -16,15 +18,18 @@ class App extends Component {
     // 사이클의 시작
     // api등 원천 데이터 획득 작업 등
     console.log('will mount');
-    this._getMapView();
-
+    
+    
   }
   
-  componentDidMount(){
+  componentDidMount= ()=>{
     // 컴포넌트들이 다 자리 잡음
+    
+    this._getLocation();
+    // this._getMapView();
     console.log('did mount');
   }
-
+  
   render(){
     // 컴포넌트가 리액트 세계에 존재
     console.log('did render');
@@ -33,9 +38,9 @@ class App extends Component {
   
     return (
       <div>
-        {/* <div id="map"></div> */}
+        <div id="map"></div>
         <div className={places ? "Place":"Place--loading"}>
-          {places ? this._renderPlaces(this.state.map) : 'Loading'}
+          {places ? this._renderPlaces() : 'Loading'}
         </div>
       </div>
     );
@@ -47,38 +52,55 @@ class App extends Component {
       center: new kakao.maps.LatLng(x, y), //지도의 중심좌표.
       level: 3 //지도의 레벨(확대, 축소 정도)
     };
-
+    
     const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-    this.setState({
-      map
-    })
-
+   
+    
     kakao.maps.event.addListener(map, 'dragend', ()=>{
-
+      
       // 지도의  레벨을 얻어옵니다
       const level = map.getLevel();
-  
+      
       // 지도의 중심좌표를 얻어옵니다 
       const latlng = map.getCenter(); 
-  
+      
       let message = '<p>지도 레벨은 ' + level + ' 이고</p>';
       message += '<p>중심 좌표는 위도 ' + latlng.getLat() + ', 경도 ' + latlng.getLng() + '입니다</p>';
-  
+      
       console.log(message);
       x = latlng.getLat();
       y = latlng.getLng();
       // console.log(getM._getMapInfo(map));
       // console.log(temp._getMapInfo(map));
-      console.log(this);
-      console.log(this._getMapInfo(map));
+      this._getMapInfo(map);
     });
-    console.log(this);
-
-
+    
+    
     this._getMapInfo(map);
   }
+  
+  _getLocation = ()=> {
+      if (navigator.geolocation) { // GPS를 지원하면
+        navigator.geolocation.getCurrentPosition( (position)=> {
+          console.log("GPS")
+          x= position.coords.latitude;
+          y= position.coords.longitude;
+          
+          console.log(position.coords.latitude + ' ' + position.coords.longitude);
+          this._getMapView();
+        },(error)=> {
+          console.error(error);
+        }, {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity
+        });
+      } else {
+        alert('GPS를 지원하지 않습니다');
+      }
 
+
+  }
 
   _getMapInfo = async(map)=>{
     const places = await this._callMapApi();
@@ -138,7 +160,7 @@ class App extends Component {
 
 
   state = {
-    greeting : 'Hello',
+    // greeting : 'Hello',
     
   }
 
@@ -155,8 +177,7 @@ class App extends Component {
     this._setMarkers(null);    
   }
   
-  _renderPlaces = (map) =>{
-    // this._getMapInfo(map);
+  _renderPlaces = () =>{
     const places = this.state.places.map((place)=>{
       return <Place
       place_name={place.place_name}
